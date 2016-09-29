f46fef8205603a7b91497fa2aa2b2c071102ad90
testica <leitotesta@gmail.com>
2016-09-29 01:20:26 -0400

    built added

--- build/main.js
+++ build/main.js
@@ -134,4 +134,9 @@ define(["require", "exports", "./bitmap"], function (require, exports, bitmap_1)
         bmp.rotate((rotateAngle * Math.PI) / 180);
         bmp.drawOnCanvas(canvas);
     });
+    document.getElementById("boxBlur").addEventListener("click", function () {
+        bmp.kernel(3, 3);
+        bmp.blur("box");
+        bmp.drawOnCanvas(canvas);
+    });
 });
